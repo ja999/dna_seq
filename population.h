b8118db9dfdea2fe0ff8c4ff5350ce01c3a80346
Aleksander Wdowiński <aleksander.wdowinski@netguru.pl>
2014-04-23 13:15:41 +0200

    Much changes. wow.

---
+++ population.h
@@ -0,0 +1,13 @@
+#ifndef POPULATION_H
+#define POPULATION_H
+
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Population {
+
+};
+
+#endif
---
+++ specimen.h
@@ -0,0 +1,16 @@
+#ifndef SPECIMEN_H
+#define SPECIMEN_H
+
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Specimen {
+  public:
+    vector<int> specimen_indexes;
+    string specimen_aligned;
+
+};
+
+#endif
